7ce263321e5018f40554aa0aba29addd7ae8bdb5
Eluan-Adumo <el1harolds@gmail.com>
2024-05-06 20:02:23 +0100

    First commit for personal portfolio website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderElement from './HeaderElement';
+
+
+
+
+
+
+function App() {
+  return (
+  
+   <HeaderElement />
+  
+  );
+}
+export default App;
---
+++ src/HeaderElement.jsx
@@ -0,0 +1,323 @@
+import React from "react";
+import myPhoto from "./images/passport.jpg";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa6";
+import { SiPhp } from "react-icons/si";
+import { IoLogoGithub, IoLogoJavascript, IoLogoPython } from "react-icons/io";
+
+const MainAppContent = ()=>{
+    return(
+        <>
+        
+        <section class = 'about-me-div'>
+            <div class = 'about-me-content'>
+                <div class = 'abm'>
+                    <h2>About me</h2>
+
+                    <p>
+                        I am a creative thinker and a go getter. I thrive in any environment that wants to push the limits and go beyond the ordinary.
+                        I love progressive challenges and never settle for anything just because it works. My personal motto is <b>More</b>.
+                    </p>
+                    <p>
+                        I&apos;m driven to be the best and to get to the top of my career.
+                    </p>
+                </div>
+
+
+                <div class = 'abm'>
+                    <h2>Hobbies</h2>
+
+                    <ul>
+                        <li>Programming</li>
+                        <li>Reading</li>
+                        <li>Music</li>
+                        <li>Learning</li>
+                        <li>Traveling</li>
+                        <li>Thinking</li>
+                    </ul>
+                </div>   
+                
+                
+
+                <div class = 'abm'>
+                    <h2>Soft skills</h2>
+
+                    <ul>
+                        <li>Speaking</li>
+                        <li>writing</li>
+                        <li>planning</li>
+                        <li>Brain storming</li>
+                        <li>Journaling</li>
+                    </ul>
+                </div>  
+                
+                
+                <div class = 'abm'>
+                    <h2>Hard Skills</h2>
+
+                    <ul>
+                        <li><FaHtml5 />&nbsp;HTML - Expert</li>
+                        <li><FaCss3Alt />&nbsp;CSS - Ninja</li>
+                        <li><SiPhp />&nbsp;PHP - Magician</li>
+                        <li><IoLogoPython  />&nbsp;PYTHON - Newbie</li>
+                        <li><IoLogoJavascript />&nbsp;JAVASCRIPT (Vanilla, Jquery, React, Node &amp; Express) - Confident</li>
+                        <li>Java - Newbie</li>
+                        <li><IoLogoGithub />&nbsp;Github - Newbie</li>
+                    </ul>
+                </div>   
+            </div>
+        </section>
+
+
+
+        <section class = 'educ-me-div'>
+            <div class = 'about-me-content'>
+                <h2 style = {{color: 'white'}}>EDUCATION</h2>
+                <div class = 'abm'>
+                    <h2>Primary Education</h2>
+
+                    <p>
+                        FUTURE LEADERS NURSERY AND PRIMARY SCHOOL (1996 - 1998)
+                     </p>
+                    <p>
+                       DON - RICI NURSERY AND PRIMARY SCHOOL (1998 - 2003)
+                    </p>
+                </div>
+
+
+                <div class = 'abm'>
+                    <h2>SECONDARY Education</h2>
+
+                    <p>
+                        BENNY INTERNATIONAL HIGH SCHOOL (2003 - 2008)
+                     </p>
+                     <p>
+                        COMMUNITY SECONDARY SCHOOL OKUTUKUTU ETEGWE BAYELSA STATE (2008) - OBTAINED WAEC &amp; NECO CERTIFICATIONS
+                     </p>
+                     <p>
+                        ATTENDED NIGER DELTA UNIVERSITY (NDU) WILBERORCE ISLAND  BAYELSA STATE.
+                     </p>
+                </div>
+                
+
+                
+                <div class = 'abm'>
+                    <h2>UNIVERSITY EDUCATION</h2>
+
+                     <p>
+                        ATTENDED NIGER DELTA UNIVERSITY (NDU) WILBERORCE ISLAND  BAYELSA STATE.
+                     </p>
+                </div>
+            </div>
+        </section>   
+        
+        <section class = 'prof-me-div'>
+            <div class = 'about-me-content'>
+                <h2 style = {{color: 'white'}}>PROFFESIONAL EXPERIENCE</h2>
+
+
+                <div class = 'abm'>
+                    <h2>Olotu Square Port harcourt</h2>
+
+                    <p>
+                       <b>ROLE:</b> Programming Instructor, Software architect and developer
+                     </p>
+                     <p>
+                        <b>DATES:</b> 2018 - 2019
+                    </p>
+                     <p>
+                        www.olotusquare.co
+                     </p>
+                </div>
+                
+
+                
+                <div class = 'abm'>
+                    <h2>Benazire Elegance</h2>
+
+                     <p>
+                        <b>Role:</b> Systems administrator
+                     </p>
+
+                     <p>
+                        <b>DATES:</b> 2020 - 2021
+                    </p>
+                     <p>
+                        Built internal operations software for job ordering, proccess management and receipts generation.
+                        These applications were used on both mobile and computer interfaces.
+                     </p>
+                </div>
+
+
+                <div class = 'abm'>
+                    <h2>Astrosoft group</h2>
+
+                     <p>
+                        <b>Role: </b> Backend Engineer, later HOD Software development deparment.
+                     </p>
+                     <p>
+                        <b>DATES: </b> 2022 - 2023
+                     </p>
+                </div>                
+            </div>
+        </section>   
+        
+        
+
+        <section class = 'free-me-div'>
+            <div class = 'about-me-content'>
+                <h2 style = {{color: 'white'}}>PORTFOLIO</h2>
+                <div class = 'abm'>
+                    <h2>Freelancing</h2>
+
+                    <p>
+                        www.electricbikeslagos.com
+                     </p>
+                    <p>
+                       www.marksmenx.com
+                    </p>
+                    <p>
+                        www.cf-main.vercel.app
+                    </p>
+                    <p>
+                        www.careful-watchers-one.vercel.app
+                    </p>
+                    <br />
+                    <h3>&mdash;&nbsp;In development&nbsp;&mdash;</h3>
+                        <br />
+                    <div class = 'prt'>
+                        <div class = 'prt-main'>
+                            <div class = 'prti' id  ='prt1'>
+
+                            </div>
+                            <div>
+                                <p>
+                                    <b>trooit.</b> a social media product
+                                </p>
+                            </div>
+                        </div>
+
+
+                        <div class = 'prt-main'>
+                            <div class = 'prti' id  ='prt2'>
+
+                            </div>
+                            <div>
+                                <p>
+                                    <b>Orbital.</b> an online store dashboard
+                                </p>
+                            </div>
+                        </div>
+
+
+                        <div class = 'prt-main'>
+                            <div class = 'prti' id  ='prt3'>
+
+                            </div>
+                            <div>
+                                <p>
+                                    <b>Shoppergram.</b> a social e-commerce platform
+                                </p>
+                            </div>
+                        </div>
+
+
+                        <div class = 'prt-main'>
+                            <div class = 'prti' id  ='prt4'>
+
+                            </div>
+
+                            <div>
+                                <p>
+                                    <b>Orbital.</b> an online store landing page
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                
+
+                
+               
+            </div>
+        </section>  
+        
+        <footer>
+            <div class = 'footer-content'>
+                &copy; David Harold 2024 &mdash; 08140186535, el1harolds@gmail.com
+            </div>
+        </footer>
+        </>
+    )
+}
+
+const WelcomeBanner = ()=>{
+
+    return(
+        <>
+                    <section class = 'welcome-banner'>
+            <div class = 'welcome-banner-content'>
+                <h1>Hello, Welcome..</h1>
+                <p>I&apos;m David</p>
+                <p class = 'small-text'>
+                    I make clean, user friendly, platform friendly and modern websites, webapps and mobile apps.
+                    Leveraging modern technologies to deliver the best results on time and on scale.
+                </p>
+                <div class = 'buttons-area'>
+                    <div class  = 'btn-gradient'>
+                        <input type = 'button' value = 'About me' />
+                    </div>
+
+                    <div class = 'btn-gradient'>
+                        <input type = 'button' value = 'Hire me' />
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+        </>
+    );
+};
+
+export default function HeaderElement(){
+    return (
+        <>
+            <header class ='p-header'>
+    <div class = 'p-header-left'>
+        <ul>
+            <li>About</li>
+            <li>Story</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    <div class = 'p-header-mid'>
+        <div class = 'colored-border'>
+            <img src = {myPhoto} class = 'logo-img' />
+        </div>
+    </div>
+    <div class = 'p-header-right'>
+        <ul>
+            <li>
+                Education
+            </li>
+            <li>
+                Proffession
+            </li>
+            <li>
+                Skills
+            </li>
+            <li>
+                Ambition
+            </li>
+        </ul>
+    </div>
+</header>
+
+
+<WelcomeBanner />
+<MainAppContent />
+        </>
+    );  
+}
